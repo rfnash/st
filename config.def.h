ecd80c620dbe795e6411bf91b9a008cc7d4ff91c
Robert F. Nash <robert@robertnash.net>
2014-04-29 14:15:18 -0400

    default font is now DejaVu Sans Mono for Powerline

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono For Powerline:pixelsize=24";
+static char font[] = "DejaVu Sans Mono for Powerline:pixelsize=24";
 static int borderpx = 2;
 static char shell[] = "/bin/sh";
 static char *utmp = NULL;
